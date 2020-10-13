53e91587f39100766b76aa1345c8355eb7390c1b
GulinAlexey <gulin-2001@mail.ru>
2020-10-14 01:20:56 +0700

    Добавлены закрытые элементы класса работников

--- Worker.java
+++ Worker.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
 
 public class Worker {
+	private int num_tr; //номер трудовой книжки
+	private String name_surname; //имя и фамилия
+	private String dolzh; //должность
+	private int hours; //кол-во рабочих часов
+	private int zarpl; //зарплата в месяц в рублях
+	private int progools; //кол-во прогулов (в днях)
 }
