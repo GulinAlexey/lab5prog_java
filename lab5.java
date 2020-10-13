a39d8b8d4bf108d437df1b4bbeb66cef1d21f687
GulinAlexey <gulin-2001@mail.ru>
2020-10-14 00:19:03 +0700

    Первый printf

---
+++ lab5.java
@@ -0,0 +1,7 @@
+public class lab5
+{
+	public static void main (String args[])
+	{
+		System.out.printf("Программа для работы с информацией о работниках заповедника и самих заповедниках.\n");
+	}
+}
