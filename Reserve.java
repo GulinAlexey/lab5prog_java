5c1e5fbf7f297c116dfb510875cf320495320ac2
GulinAlexey <gulin-2001@mail.ru>
2020-10-14 01:29:40 +0700

    Добавлены закрытые элементы в класс заповедников, числовые переменные инициализируются нулевыми

--- Reserve.java
+++ Reserve.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
 
 public class Reserve {
+	private static final int LEN = 100; //макс. число работников заповедника
+	private String title;  //название заповедника
+	private int budget=0;          //бюджет заповедника
+	private int expens=0;          //расходы
+	private int kolvow=0;         //кол-во работников в заповеднике
+	private Worker[] workers = new Worker[LEN]; //работники заповедника
 }
--- Worker.java
+++ Worker.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Worker {
-	private int num_tr; //номер трудовой книжки
+	private int num_tr=0; //номер трудовой книжки
 	private String name_surname; //имя и фамилия
 	private String dolzh; //должность
-	private int hours; //кол-во рабочих часов
-	private int zarpl; //зарплата в месяц в рублях
-	private int progools; //кол-во прогулов (в днях)
+	private int hours=0; //кол-во рабочих часов
+	private int zarpl=0; //зарплата в месяц в рублях
+	private int progools=0; //кол-во прогулов (в днях)
 }
